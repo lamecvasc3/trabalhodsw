a90be91b99d42ea097459d284c2fb2c2f8dbd787
Lamec Vasconcelos <josuelamec@hotmail.com>
2021-08-11 16:14:50 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { PermanentDrawerLeft } from "./components/SideBar/PermanentDrawerLeft";
+
+function App() {
+  return (
+    <div>
+      <PermanentDrawerLeft></PermanentDrawerLeft>
+    </div>
+  );
+}
+
+export default App;
